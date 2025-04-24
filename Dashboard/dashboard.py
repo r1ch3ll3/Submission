1857d8969838d0c51646cc535d9dcdbeab7569a5
Richelle Vania Thionanda <richellevaniatn@gmail.com>
2025-04-24 14:58:55 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
+from PIL import Image
 
 # Load dataset
 hour_df = pd.read_csv("Data/hour.csv")
